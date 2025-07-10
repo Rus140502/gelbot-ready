30cdb81c795fb0ac655e8362370ff5fb97aca169
Rus140502 <tashkalov2011@mail.ru>
2025-07-10 17:52:56 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,30 @@
+import os
+import asyncio
+import aiosqlite
+import threading
+from flask import Flask
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+app_flask = Flask(__name__)
+
+@app_flask.route('/')
+def home():
+    return "Бот работает!"
+
+def run_keepalive():
+    app_flask.run(host="0.0.0.0", port=8080)
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет! Я бот по приему заказов.")
+
+async def main():
+    application = Application.builder().token(BOT_TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    threading.Thread(target=run_keepalive).start()
+    await application.run_polling()
+
+if __name__ == "__main__":
+    asyncio.run(main())
